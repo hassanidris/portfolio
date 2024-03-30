1e5db6d44e85eb2bab42d1d33dd669cfcdbc6dbb
Hassen Ahmed <hassan.creative@gmail.com>
2024-03-30 07:43:09 +0100

    fixing keys

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -18,16 +18,6 @@ const Skills = () => {
       <SkillText />
       <div className=" mt-4 flex flex-row flex-wrap items-center justify-center gap-5">
         {Frontend_skill.map((image, index) => (
-          //   <div
-          //     key={index}
-          //     className=" relative z-10
-          //  before:absolute
-          //   before:-top-3 before:left-1/2 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-md before:bg-white before:px-3 before:py-2 before:text-black before:opacity-0 before:transition-all before:content-[attr(data-tip)]
-          //   after:absolute after:-top-3 after:left-1/2 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-transparent after:border-l-transparent after:border-r-transparent after:border-t-white after:opacity-0 after:transition-all
-
-          //    hover:before:opacity-100 hover:after:opacity-100"
-          //     data-tip={image.skill_name}
-          //   >
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -36,21 +26,10 @@ const Skills = () => {
             skill={image.skill_name}
             index={index}
           />
-          // </div>
         ))}
       </div>
       <div className="mt-4 flex flex-row flex-wrap items-center justify-center gap-5">
         {Backend_skill.map((image, index) => (
-          //     <div
-          //       key={index}
-          //       className=" relative z-10
-          //  before:absolute
-          //   before:-top-3 before:left-1/2 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-md before:bg-white before:px-3 before:py-2 before:text-black before:opacity-0 before:transition-all before:content-[attr(data-tip)]
-          //   after:absolute after:-top-3 after:left-1/2 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-transparent after:border-l-transparent after:border-r-transparent after:border-t-white after:opacity-0 after:transition-all
-
-          //    hover:before:opacity-100 hover:after:opacity-100"
-          //       data-tip={image.skill_name}
-          //     >
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -59,21 +38,10 @@ const Skills = () => {
             skill={image.skill_name}
             index={index}
           />
-          // </div>
         ))}
       </div>
       <div className="mt-4 flex flex-row flex-wrap items-center justify-center gap-5">
         {Design_skill.map((image, index) => (
-          //     <div
-          //       key={index}
-          //       className=" relative z-10
-          //  before:absolute
-          //   before:-top-3 before:left-1/2 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-md before:bg-white before:px-3 before:py-2 before:text-black before:opacity-0 before:transition-all before:content-[attr(data-tip)]
-          //   after:absolute after:-top-3 after:left-1/2 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-transparent after:border-l-transparent after:border-r-transparent after:border-t-white after:opacity-0 after:transition-all
-
-          //    hover:before:opacity-100 hover:after:opacity-100"
-          //       data-tip={image.skill_name}
-          //     >
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -82,21 +50,10 @@ const Skills = () => {
             skill={image.skill_name}
             index={index}
           />
-          // </div>
         ))}
       </div>
       <div className="mt-4 flex flex-row flex-wrap items-center justify-center gap-5">
         {Other_skill.map((image, index) => (
-          //     <div
-          //       key={index}
-          //       className=" relative z-10
-          //  before:absolute
-          //   before:-top-3 before:left-1/2 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-md before:bg-white before:px-3 before:py-2 before:text-black before:opacity-0 before:transition-all before:content-[attr(data-tip)]
-          //   after:absolute after:-top-3 after:left-1/2 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-transparent after:border-l-transparent after:border-r-transparent after:border-t-white after:opacity-0 after:transition-all
-
-          //    hover:before:opacity-100 hover:after:opacity-100"
-          //       data-tip={image.skill_name}
-          //     >
           <SkillDataProvider
             key={index}
             src={image.Image}
