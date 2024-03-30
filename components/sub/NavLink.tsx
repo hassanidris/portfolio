a084227f7041984186978316bddbf7f10d3020cf
Hassen Ahmed <hassan.creative@gmail.com>
2024-03-30 07:47:24 +0100

    fixing keys #2

--- components/sub/NavLink.tsx
+++ components/sub/NavLink.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-const NavLink = ({ href, title }) => {
-  return (
-    <Link
-      href={href}
-      className="block rounded py-2 pl-3 pr-4 text-[#ADB7BE] hover:text-white sm:text-xl md:p-0"
-    >
-      {title}
-    </Link>
-  );
-};
+// const NavLink = ({ href, title }) => {
+//   return (
+//     <Link
+//       href={href}
+//       className="block rounded py-2 pl-3 pr-4 text-[#ADB7BE] hover:text-white sm:text-xl md:p-0"
+//     >
+//       {title}
+//     </Link>
+//   );
+// };
 
-export default NavLink;
+// export default NavLink;
